870ddbc0b71104ae17936c9ad3980c92dfdf11de
L <rongtangli@hengtiansoft.com>
2020-07-22 10:32:06 +0800

    前端--环境安装

--- sorce_web/src/main.js
+++ sorce_web/src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import axios from 'axios'                       //axios
+import ElementUI from 'element-ui';             //Element UI      
+import 'element-ui/lib/theme-chalk/index.css';  //Element UI    
+import VueHighlightJS from 'vue-highlightjs'    //highlight
+import 'highlight.js/styles/atom-one-dark.css'  //highlight
 
+
+
+Vue.prototype.$axios = axios  
 Vue.config.productionTip = false
+Vue.use(ElementUI)
+Vue.use(VueHighlightJS)
+
 
 new Vue({
   router,
