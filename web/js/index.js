69e7835435c559c84e5260c0700d3956c8590580
L <rongtangli@hengtiansoft.com>
2020-07-23 13:05:21 +0800

    移动端菜单

--- web/js/index.js
+++ web/js/index.js
@@ -1,6 +1,8 @@
 let active = col_id;
-//菜单
-function menuFun(){
+
+//菜单PC端
+function menuFunPC(){
+
     if(active == 'home'){
         $('.one-menu-li a').css('color','white');
     }else if(active == 'blog'){
@@ -8,6 +10,7 @@ function menuFun(){
     }else{
         $('.one-menu-li:nth-child(3)').css('color','white');
     }
+
     $('.one-menu-li').mouseenter(function(){
         if($(this).find('.two-menu')){
             $(this).find('.two-menu').show();
@@ -16,4 +19,35 @@ function menuFun(){
         $('.two-menu').hide();
     });
 }
-menuFun();
+
+
+//菜单移动端
+function menuFunMOB(){
+    $('.one-menu-mob li').click(function(){     //点击一级菜单
+        // if($(this).find('.two-menu-mob')){          
+        //     $(this).addClass('down');           //箭头向下
+        //     $(this).find('.two-menu-mob').toggle(); //二级菜单
+        // }
+
+        // if($(this).find('.two-menu-mob').hide()){
+        //     $(this).removeClass('down');           
+        // }
+
+        // if($(this).find('.two-menu-mob').hide()){
+        //     $(this).addClass('down');           //箭头向下
+        //     $(this).find('.two-menu-mob').toggle();
+        // }else{
+        //     $(this).removeClass('down');           //箭头向下
+        //     $(this).find('.two-menu-mob').toggle();
+        // }
+    });
+
+    $('.menu-bt img,.cancel-bt img').click(function(){
+        $('.head-mob-row2').toggle();
+    });
+    
+    
+}
+
+menuFunPC();
+menuFunMOB();
