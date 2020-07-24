2e601d3ff62491c625725691738bd116e62bd836
L <rongtangli@hengtiansoft.com>
2020-07-24 13:26:09 +0800

    禁止右键 F12 复制

--- web/js/index.js
+++ web/js/index.js
@@ -1,29 +1,29 @@
 let active = col_id;
 
 //菜单PC端
-function menuFunPC(){
+function menuFunPC() {
 
-    if(active == 'home'){
-        $('.one-menu-li a').css('color','white');
-    }else if(active == 'blog'){
-        $('.one-menu-li:nth-child(2)').css('color','white');
-    }else{
-        $('.one-menu-li:nth-child(3)').css('color','white');
+    if (active == 'home') {
+        $('.one-menu-li a').css('color', 'white');
+    } else if (active == 'blog') {
+        $('.one-menu-li:nth-child(2)').css('color', 'white');
+    } else {
+        $('.one-menu-li:nth-child(3)').css('color', 'white');
     }
 
-    $('.one-menu-li').mouseenter(function(){
-        if($(this).find('.two-menu')){
+    $('.one-menu-li').mouseenter(function () {
+        if ($(this).find('.two-menu')) {
             $(this).find('.two-menu').show();
         }
-    }).mouseleave(function(){
+    }).mouseleave(function () {
         $('.two-menu').hide();
     });
 }
 
 
 //菜单移动端
-function menuFunMOB(){
-    $('.one-menu-mob li').click(function(){     //点击一级菜单
+function menuFunMOB() {
+    $('.one-menu-mob li').click(function () {     //点击一级菜单
         // if($(this).find('.two-menu-mob')){          
         //     $(this).addClass('down');           //箭头向下
         //     $(this).find('.two-menu-mob').toggle(); //二级菜单
@@ -42,81 +42,81 @@ function menuFunMOB(){
         // }
     });
 
-    $('.menu-bt img,.cancel-bt img').click(function(){
+    $('.menu-bt img,.cancel-bt img').click(function () {
         $('.head-mob-row2').toggle();
     });
-    
-    
+
+
 }
 
 //PC登录
-var loginFun = function(){
+var loginFun = function () {
     //取消按钮
-    $('.cancel-login').click(function(){        
-        $('.logindiv').animate({'top':'-400px'},'slow');
+    $('.cancel-login').click(function () {
+        $('.logindiv').animate({ 'top': '-400px' }, 'slow');
         setTimeout(() => {
             $('.loginspan,.pwd2,.code-bt,.regin-bt,.submit-bt').hide();
             $('.reginspan,.pwd,.login-bt,.loginCode-bt').show();
-        },2000);
+        }, 2000);
     });
 
     //登录框弹出动画
-    $('.login-btn').click(function(){
-        $('.logindiv').animate({'top':'100px'},'slow');
+    $('.login-btn').click(function () {
+        $('.logindiv').animate({ 'top': '100px' }, 'slow');
     });
 
     //登录框背景放大缩小动画
-    $('.logindiv').mouseenter(function(){
-        $(this).animate({'background-size':'110%'},'fast');
-    }).mouseleave(function(){
-        $(this).animate({'background-size':'100%'},'fast');
+    $('.logindiv').mouseenter(function () {
+        $(this).animate({ 'background-size': '110%' }, 'fast');
+    }).mouseleave(function () {
+        $(this).animate({ 'background-size': '100%' }, 'fast');
     });
 
     //点击注册 链接
-    $('.regin-a').click(function(){
+    $('.regin-a').click(function () {
         $('.login-bt,.reginspan,.loginCode-bt,.submit-bt,.code-bt2').hide();
         $('.regin-bt,.pwd,.pwd2,.code-bt,.loginspan').show();
     });
     //点击登录 链接
-    $('.login-a').click(function(){
+    $('.login-a').click(function () {
         $('.loginspan,.pwd2,.code-bt,.regin-bt,.submit-bt,.code-bt2').hide();
         $('.reginspan,.pwd,.login-bt,.loginCode-bt').show();
     });
 
-     //点击忘记密码 链接
-     $('.lock').click(function(){
+    //点击忘记密码 链接
+    $('.lock').click(function () {
         $('.pwd,.pwd2,.loginCode-bt,.login-bt,.regin-bt,.code-bt').hide();
         $('.submit-bt,.code-bt2').show();
     });
 
     //点击获取邮箱验证码 注册
-    $('.getCode').click(function(){
+    $('.getCode').click(function () {
         let i = 60;
         $('.getCode').addClass('stopClick');
-        let tim = setInterval(function(){
-            i = i-1;
+        let tim = setInterval(function () {
+            i = i - 1;
             $('.getCode').html(i + '秒后获取');
-            if(i == -1){
+            if (i == -1) {
                 $('.getCode').html('获取验证码');
                 $('.getCode').removeClass('stopClick');
                 clearInterval(tim);
             }
-        },1000);
+        }, 1000);
     });
 
-     //点击获取邮箱验证码 忘记密码
-     $('.getCode2').click(function(){
+    //点击获取邮箱验证码 忘记密码
+    $('.getCode2').click(function () {
         let i = 60;
         $('.getCode2').addClass('stopClick');
-        let tim = setInterval(function(){
-            i = i-1;
+        let tim = setInterval(function () {
+            i = i - 1;
             $('.getCode2').html(i + '秒后获取');
-            if(i == -1){
+            if (i == -1) {
                 $('.getCode2').html('获取验证码');
                 $('.getCode2').removeClass('stopClick');
                 clearInterval(tim);
             }
-        },1000);
+        }, 1000);
     });
 }
 
@@ -125,3 +125,20 @@ var loginFun = function(){
 menuFunPC();
 menuFunMOB();
 loginFun();
+
+
+
+//禁止右键
+// document.oncontextmenu=function(){
+//     return false;
+// }
+
+//禁止 F12
+// document.onkeydown=function (e){
+//     var currKey=0,evt=e||window.event;
+//     currKey=evt.keyCode||evt.which||evt.charCode;
+//     if (currKey == 123) {
+//         window.event.cancelBubble = true;
+//         window.event.returnValue = false;
+//     }
+// }
